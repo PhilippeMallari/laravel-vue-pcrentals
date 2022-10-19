593ada6a1583dd75c52e92a94a81531faf67843c
PhilippeMallari <56430090+PhilippeMallari@users.noreply.github.com>
2022-10-20 01:47:10 +0800

    first commit

---
+++ vue/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+//import {mapState} from 'vuex'
+import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   components: {
+//     HelloWorld
+//   },
+//   computed: {
+//     ...mapState(['user'])
+//   }
+// }
+</script>
+
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+
+</style>
