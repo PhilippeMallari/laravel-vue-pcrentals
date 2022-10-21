e881a8d8ef48de683e91fb34b04eb1ab3ae32213
PhilippeMallari <56430090+PhilippeMallari@users.noreply.github.com>
2022-10-22 05:47:10 +0800

    added the views in vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,20 +1,9 @@
 <script setup>
-//import {mapState} from 'vuex'
-import HelloWorld from './components/HelloWorld.vue'
 
-// export default {
-//   components: {
-//     HelloWorld
-//   },
-//   computed: {
-//     ...mapState(['user'])
-//   }
-// }
 </script>
 
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <router-view></router-view>
 </template>
 
 <style>
