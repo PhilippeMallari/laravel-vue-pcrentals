683df52dda3441ad62bb5268dcc83258053fb49b
PhilippeMallari <mallari.philippe@gmail.com>
2022-10-24 00:25:23 +0800

    Added additional enhancements to views
    -Added input for register.vue
    -Added router links
    -Handled user token when logged in or not

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,16 +2,19 @@ import { createRouter, createWebHistory } from "vue-router"
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
-import Surveys from "../views/Surveys.vue"
+import Surveys from "../views/Surveys.vue";
 import FrequentlyAskedQuestions from "../views/FrequentlyAskedQuestions.vue";
 import ComputerForSale from "../views/ComputerForSale.vue";
-import DefaultLayout from "../components/DefaultLayout.vue"
+import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
+import store from "../store";
 
 const routes = [
     {
         path: '/',
         redirect: '/dashboard',
         component: DefaultLayout,
+        meta: { requiresAuth: true },
         children: [
             {
                 path: '/dashboard',
@@ -26,14 +29,23 @@ const routes = [
         ]
     },
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        component: AuthLayout,
+        meta: { isGuest: true },
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: Register
+            }
+        ]
     },
     {
         path: '/faq',
@@ -50,6 +62,16 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: 'Login' })
+    } else if (store.state.user.token && (to.meta.isGuest)) {
+        next({ name: 'Dashboard' });
+    } else {
+        next()
+    }
 })
 
 export default router;
--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -3,7 +3,6 @@
         <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
             <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
         </div>
-    </header>
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <!-- Replace with your content -->
@@ -13,6 +12,7 @@
             <!-- /End replace -->
         </div>
     </main>
+    </header>
 </template>
 
 <script>
